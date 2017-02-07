ad29eb89ceb46ad6976c2e647780cf14c9193db7
Shpetim Haxhiu <shpetim.h@gmail.com>
2017-02-07 11:22:03 +0100

    a

--- js/app.js
+++
@@ -1,8 +0,0 @@
-$(window).load(
-	FB.getLoginStatus(function(response) {
-    if(response.status== "connected") {
-    	console.log("User is logged in");
-    	self.location = "http://www.facebook.com";
-    }
-    console.log(response);
-}));
