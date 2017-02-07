1963846d39d581a8649ac442eb4bb9f64b0bcda4
Shpetim Haxhiu <shpetim.h@gmail.com>
2017-02-07 11:54:32 +0100

    Version 3

--- js/app.js
+++ js/app.js
@@ -48,6 +48,60 @@ function addPlusMeController($scope) {
 		console.log('Total Countable Dishes: ' + dishesNo);
 
 	};
-}
+
+
+	$scope.statusChangeCallback = function(response) {
+	 	console.log('statusChangeCallback');
+	    console.log(response);
+	    if (response.status === 'connected') {
+	      testAPI();
+
+	    } else if (response.status === 'not_authorized') {
+	      FB.login(function(response) {
+	        statusChangeCallback2(response);
+	      }, {scope: 'public_profile,email'});
+
+	    } else {
+	      alert("not connected, not logged into facebook, we don't know");
+	    }
+	}
+
+
+	$scope.statusChangeCallback2 = function(response) {
+	    console.log('statusChangeCallback2');
+	    console.log(response);
+	    if (response.status === 'connected') {
+	      testAPI();
+
+	    } else if (response.status === 'not_authorized') {
+	      console.log('still not authorized!');
+
+	    } else {
+	      alert("not connected, not logged into facebook, we don't know");
+	    }
+	}
+
+	$scope.checkLoginState = function() {
+	    FB.getLoginStatus(function(response) {
+	      statusChangeCallback(response);
+	    });
+	}
+
+	$scope.testAPI = function(){
+		console.log('Welcome!  Fetching your information.... ');
+	    FB.api('/me', function(response) {
+    		alert('Tungjat ' + response.name)
+	      	console.log('Successful login for: ' + response.name);
+	      	document.getElementById('status').innerHTML =
+	        'Thanks for logging in, ' + response.name + '!';
+	    });
+	}
+
+	$scope.onButtonClick = function() {
+		// Add this to a button's onclick handler
+		FB.AppEvents.logEvent("sentFriendRequest");
+		console.log("Event logged");
+	}
+	}
 
 })();
