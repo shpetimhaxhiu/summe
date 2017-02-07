a9d007784861842e0c1bb66b9efd42e8a9376535
Shpetim Haxhiu <shpetim.h@gmail.com>
2017-02-07 11:16:54 +0100

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
     if(response.status== "connected") {
     	console.log("User is logged in");
     	self.location = "http://www.facebook.com";
